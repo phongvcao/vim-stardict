655fddd2fd963e91ec36e1d410071156d1bdb079
Phong V. Cao <phongvcao@phongvcao.com>
2015-02-25 20:07:25 -0500

    - Finished bash commandline implementation

--- bash/stardict.sh
+++ bash/stardict.sh
@@ -1,12 +1,21 @@
-function stardict() {
-if [[ "$1" == "-e" || "$1" == "--editor" ]]; then
-    PYSCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd ../python && pwd)
-    PYTHON_COMMAND="import sys; sys.path.insert(0, '"${PYSCRIPT_DIR}"')"
-    VIM_COMMAND="setlocal buftype=nofile bufhidden=hide noswapfile readonly filetype=stardict"
-    python -c "${PYTHON_COMMAND}; import stardict; print(stardict.getDefinition([['$2']]))" | vim -c "${VIM_COMMAND}" -
-else
-    for arg in "$@"; do
-        $( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd ../python && pwd)/stardict.py "${arg}"
-    done
-fi
+function bsdcv() {
+for arg in "$@"; do
+    $( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd ../python && pwd)/stardict.py "${arg}"
+done
+}
+
+function vsdcv() {
+PYSCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd ../python && pwd)
+PYTHON_COMMAND="import sys; sys.path.insert(0, '"${PYSCRIPT_DIR}"')"
+VIM_COMMAND="setlocal buftype=nofile bufhidden=hide noswapfile readonly filetype=stardict"
+
+FINAL_DEFINITIONS=""
+ARGS_LIST=""
+
+for arg in "$@"; do
+    FINAL_DEFINITIONS+="$(python -c "${PYTHON_COMMAND}; import stardict; print(stardict.getDefinition([['$arg']]))")""\n""\n"
+done
+
+echo -e "${FINAL_DEFINITIONS}"
+echo -e "${FINAL_DEFINITIONS}" | vim -c "${VIM_COMMAND}" -
 }
