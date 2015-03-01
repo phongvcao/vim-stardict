83dd6ea80b7e4c1866de0b89677597a2e69b63b0
Phong V. Cao <phongvcao@phongvcao.com>
2015-02-28 22:45:02 -0500

    - Added inputable

--- python/stardict.py
+++ python/stardict.py
@@ -201,8 +201,8 @@ def formatStr(outputStr):
                     replacedStr = re.sub("^\\-", "        -", line,
                             flags=re.IGNORECASE)
                 else:
-                    replacedStr = re.sub("^\\-\\s+", str(replacedBullet) + ". ",
-                            line, flags=re.IGNORECASE)
+                    replacedStr = re.sub("^\\-\\s+", str(replacedBullet)
+                            ". ", line, flags=re.IGNORECASE)
                     replacedBullet += 1
             finalStr += replacedStr
 
